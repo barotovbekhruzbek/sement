f61a2228bcf2dce25a1a6f16d6c4590dde116fa9
barotovbekhruzbek <barotovbekhruzbek@gmail.com>
2023-09-12 09:04:18 +0500

    contact info

---
+++ src/views/home/componenets/Contact-info.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="contact-info">
+    <div class="container">
+        <div class="wrapper">
+            <div class="box">
+                <div class="logo">
+                    <img src="../../../assets/images/card-sement/person.png" alt="">
+                </div>
+                <div class="content">
+                    <h3>О'z sohasidagi
+mutaxassislar</h3>
+                </div>
+            </div>
+            <div class="box">
+                <div class="logo">
+                    <img src="../../../assets/images/card-sement/cars.png" alt="">
+                </div>
+                <div class="content">
+                    <h3>Yetkazib berish xizmati</h3>
+                </div>
+            </div>
+            <div class="box">
+                <div class="logo">
+                    <img src="../../../assets/images/card-sement/xodim.png" alt="">
+                </div>
+                <div class="content">
+                    <h3>600 dan ortiq xodimlar</h3>
+                </div>
+            </div>
+        </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+  .container{
+        max-width: 1440px;
+        margin: 0 auto;
+        @media (max-width:1400px) {
+            max-width: 900px;
+    }
+}
+.wrapper {
+    display: flex;
+    justify-content: space-around;
+}
+</style>
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,15 +1,18 @@
 <template>
   <Carusel />
   <SementAbout />
+  <contact-info />
 </template>
 
 <script>
 import Carusel from './componenets/Carusel.vue'
+import ContactInfo from './componenets/Contact-info.vue'
 import SementAbout from './componenets/SementAbout.vue'
 export default {
     components:{
         Carusel,
-        SementAbout
+        SementAbout,
+        ContactInfo
     }
 }
 </script>
